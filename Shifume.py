dd90e7e3a75aee0adb57bf3e25daf7b4ea3eb5c3
Emira_Harrioui <harrioui.e@gmail.com>
2019-11-07 14:14:41 +0100

    Add rules of choice of values

--- Shifume.py
+++ Shifume.py
@@ -6,11 +6,20 @@ def add_Name():
     name_player = input("Merci de renseigner votre nom: ")
 
     while len(name_player) == 0:
+
         msg_noname = "Vous n'avez rien renseigné"
         print(msg_noname)
+
         name_player = input("Merci de renseigner votre nom: ")
 
-    print("Bienvenue {} sur le jeu SHIFUMI!".format(name_player))
+    msg_welcome = "Bienvenue {} sur le jeu SHIFUMI!".format(name_player)
+
+    print(msg_welcome)
+
+    msg_rule = "Pour jouer, il vous suffit de taper la première lettre de votre choix. Pour choisir pierre, feuille ou ciseaux, veuillez taper respectivement sur la touche P, F ou C"
+
+    print(msg_rule)
+
     return name_player
 
 name=add_Name()

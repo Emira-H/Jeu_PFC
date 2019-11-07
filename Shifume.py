e2da967e8a5c3e08df7cd47f7373266f62b3b5b9
Emira_Harrioui <harrioui.e@gmail.com>
2019-11-07 16:04:16 +0100

    add function choice value

--- Shifume.py
+++ Shifume.py
@@ -11,10 +11,11 @@ def add_Name():
         print(msg_noname)
 
         name_player = input("Merci de renseigner votre nom: ")
-
+#message to welcome the player
     msg_welcome = "Bienvenue {} sur le jeu SHIFUMI!".format(name_player)
 
     print(msg_welcome)
+#message to explain how to do the choice
 
     msg_rule = "Pour jouer, il vous suffit de taper la première lettre de votre choix. Pour choisir pierre, feuille ou ciseaux, veuillez taper respectivement sur la touche P, F ou C"
 
@@ -23,3 +24,26 @@ def add_Name():
     return name_player
 
 name=add_Name()
+
+
+#Choice of value
+
+def Value_player():
+    choice = input("Entrer P, F ou C! A vous de jouer! ").upper()
+
+    while choice not in ["P","F","C"]:
+        print("choix invalide")
+        choice = input("Entrer P, F ou C! A vous de jouer! ").upper()
+
+    if choice == "P":
+        print("Vous avez choisi PIERRE")
+
+    elif choice == "F":
+        print("vous avez choisi FEUILLE")
+
+    elif choice == "C":
+        print("vous avez choisi CISEAUX")
+
+    return choice
+
+choix = Value_player()
